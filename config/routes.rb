ca85076c5861355bf627f4e2f553d46acada7bbd
Parth Chugh <parthchugh52@gmail.com>
2020-01-20 14:37:55 +0530

    changed configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
+  get 'posts/:id/edit', to: 'hello_world#index';
+  get 'posts/:id', to: 'hello_world#index';
+  get 'posts/new', to: 'hello_world#index';
+  root 'hello_world#index'
 end
